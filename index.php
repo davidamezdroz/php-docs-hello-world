d78ac37de64ff14b5e3d39b50dbe542d19b920e1
davidamezdroz <132052370+davidamezdroz@users.noreply.github.com>
2023-08-07 16:35:08 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,10 @@
 <?php
-require_once 'vendor/autoload.php'; // Assurez-vous que le chemin vers l'autoloader de la bibliothèque Azure est correct
-
-use MicrosoftAzure\Storage\Common\ServicesBuilder;
-
 $accountName = "swordsa1";
 $accountKey = "TL96yoiEXDITU/gPUYY1AnMeSE9qQjyJvMKJ7XJl4RbpRXvyYf3LnsLro4whtfVXfZPUuLggOfp5+ASt5FWn7g==";
 $containerName = "test";
 
 $connectionString = "DefaultEndpointsProtocol=https;AccountName=$accountName;AccountKey=$accountKey;EndpointSuffix=core.windows.net";
-$blobClient = ServicesBuilder::getInstance()->createBlobService($connectionString);
+$blobClient = \WindowsAzure\Common\ServicesBuilder::getInstance()->createBlobService($connectionString);
 
 $blobs = $blobClient->listBlobs($containerName);
 
