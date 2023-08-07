08b02763926265b259506b149a3475a281905f5b
davidamezdroz <132052370+davidamezdroz@users.noreply.github.com>
2023-08-07 16:37:55 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,3 @@
 <?php
-$accountName = "swordsa1";
-$accountKey = "TL96yoiEXDITU/gPUYY1AnMeSE9qQjyJvMKJ7XJl4RbpRXvyYf3LnsLro4whtfVXfZPUuLggOfp5+ASt5FWn7g==";
-$containerName = "test";
-
-$connectionString = "DefaultEndpointsProtocol=https;AccountName=$accountName;AccountKey=$accountKey;EndpointSuffix=core.windows.net";
-$blobClient = \WindowsAzure\Common\ServicesBuilder::getInstance()->createBlobService($connectionString);
-
-$blobs = $blobClient->listBlobs($containerName);
-
-echo "<h1>Liste des fichiers dans le conteneur $containerName :</h1>";
-echo "<ul>";
-foreach ($blobs as $blob) {
-    echo "<li>" . $blob->getName() . "</li>";
-}
-echo "</ul>";
+echo "Hello";
 ?>
